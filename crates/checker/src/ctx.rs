d4d4e0682985846d38555792af68dd60504f3f84
Maxwell Elliot Heiber <mheiber@fb.com>
2022-06-29 23:11:57 +0100

    test Ctx scopes properly

--- crates/checker/src/ctx.rs
+++ crates/checker/src/ctx.rs
@@ -61,3 +61,30 @@ impl<'a> Ctx<'a> {
         self.look_for.as_ref() == Some(expr_with_pos)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::data::Ty;
+
+    fn x() -> String {
+        "x".to_string()
+    }
+
+    #[test]
+    fn test_scopes() {
+        let mut diagnostics = Vec::new();
+        let mut scopes = Vec::new();
+        let mut ctx = Ctx::new(&mut diagnostics, &mut scopes, true, &None);
+        ctx.enter(&mut |ctx| {
+            ctx.insert(x(), Ty::Bool);
+            assert!(ctx.get("x").unwrap() == &Ty::Bool);
+            ctx.enter(&mut |ctx| {
+                assert!(ctx.get("x").unwrap() == &Ty::Bool);
+                ctx.insert(x(), Ty::Num);
+                assert!(ctx.get("x").unwrap() == &Ty::Num);
+            });
+            assert!(ctx.get("x").unwrap() == &Ty::Bool);
+        });
+    }
+}
